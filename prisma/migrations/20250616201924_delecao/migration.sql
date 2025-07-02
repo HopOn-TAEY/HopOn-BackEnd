69756761ce6a3ec21502c9f9b0c6b59da1041c88
José Eric <80716718+JESP2310@users.noreply.github.com>
2025-07-02 15:37:58 -0300

    Revert "Fix"

--- prisma/migrations/20250616201924_delecao/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `id_passageiro` on the `Corrida` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Corrida" DROP COLUMN "id_passageiro";
